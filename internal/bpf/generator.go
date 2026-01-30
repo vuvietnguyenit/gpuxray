66d8c653e5b31624b37ebae8b97060cea63fb7b5
vunv <vunv@coccoc.com>
2026-01-30 17:20:15 +0700

    update license

--- internal/bpf/generator.go
+++ internal/bpf/generator.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Vu Nguyen
+
 package bpf
 
 //go:generate sh -c "bpftool btf dump file /sys/kernel/btf/vmlinux format c > headers/vmlinux.h"
--- internal/memtrace/generate.go
+++ internal/memtrace/generate.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Vu Nguyen
+
 package memtrace
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -output-dir gen -go-package gen Memdriver ./bpf/memdriver.bpf.c -- -I../../bpf/headers -D__TARGET_ARCH_x86
