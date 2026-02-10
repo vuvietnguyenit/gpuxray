ca906e1336e6a439ddf0e021c9bd6c176e04a8b8
vunv <vunv@coccoc.com>
2026-02-10 17:12:06 +0700

    clean cache

--- internal/lifecycle/generator.go
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2026 Vu Nguyen
-
-package lifecycle
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -output-dir gen -go-package gen ProcessExit ./bpf/process_exit.bpf.c -- -I../../bpf/headers -D__TARGET_ARCH_x86
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -output-dir gen -go-package gen Cuda ./bpf/cuda.bpf.c -- -I../../bpf/headers -D__TARGET_ARCH_x86
