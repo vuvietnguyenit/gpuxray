9ea938de7496b0e9c32b6b655347f143b5a65f4b
vunv <vunv@coccoc.com>
2026-03-02 14:46:53 +0700

    deprecated old way

--- internal/memtrace/controller.go
+++ internal/memtrace/controller.go
@@ -5,8 +5,6 @@ package memtrace
 
 import (
 	"context"
-
-	"github.com/vuvietnguyenit/gpuxray/internal"
 )
 
 type Config struct {
@@ -18,13 +16,7 @@ func runGPUMemoryTracer(ctx context.Context, objs *Objects, cfg Config) error {
 	tracer := NewGPUMemoryTracer(objs, cfg)
 	return tracer.Run(ctx)
 }
-func runGPUStackTracer(ctx context.Context, objs *Objects, cfg Config) error {
-	tracer := NewGPUStackTrace(objs, cfg)
-	return tracer.Run(ctx)
-}
+
 func Run(ctx context.Context, objs *Objects, cfg Config) error {
-	if internal.MemoryleakFlags.PrintStack {
-		return runGPUStackTracer(ctx, objs, cfg)
-	}
 	return runGPUMemoryTracer(ctx, objs, cfg)
 }
