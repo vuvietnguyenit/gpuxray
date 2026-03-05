f0504c80bfe95a59f651cd03af06a4353a8f4a3b
vunv <vunv@coccoc.com>
2026-03-05 17:59:56 +0700

    add install script

--- install.sh
+++ install.sh
@@ -9,30 +9,20 @@ OS=$(uname | tr '[:upper:]' '[:lower:]')
 # Detect ARCH
 ARCH=$(uname -m)
 
-case "$ARCH" in
-    x86_64)
-        ARCH="amd64"
-        ;;
-    aarch64|arm64)
-        ARCH="arm64"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
-esac
-
 # Get latest version from GitHub
 VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep tag_name | cut -d '"' -f4)
+echo $VERSION
 
 FILE="gpuxray_${OS}_${ARCH}.tar.gz"
-URL="https://github.com/$REPO/releases/download/${VERSION}/${FILE}"
+URL="https://github.com/$REPO/releases/download/v0.1.1/${FILE}"
+echo $URL
 
 echo "Installing gpuxray $VERSION for $OS/$ARCH"
 
 TMP_DIR=$(mktemp -d)
 
 curl -L "$URL" -o "$TMP_DIR/gpuxray.tar.gz"
+echo $TMP_DIR
 
 tar -xzf "$TMP_DIR/gpuxray.tar.gz" -C "$TMP_DIR"
 
